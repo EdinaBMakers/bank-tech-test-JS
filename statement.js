7e1185fbbedff0d6c7972698991416186f5f7c69
Edina Bolkeny <bolkeny.edina@gmail.com>
2019-10-24 16:42:08 +0100

    Test and implement print

--- statement.js
+++ statement.js
@@ -10,7 +10,18 @@ class Statement {
   }
 
   print() {
-    return 'date || credit || debit || balanc';
+    const header = 'date || credit || debit || balance';
+    const reversedTransactions = this.transactions.slice().reverse();
+    let balance = this.balance;
+
+    const transactionsStr = reversedTransactions.map(trans => {
+      let dateStr = trans.date.getDate() + '/' + trans.date.getMonth() + '/' + trans.date.getFullYear();
+      let str = `${dateStr} || ${trans.credit} || ${trans.debit} || ${balance}`;
+      balance += (trans.debit - trans.credit);
+      return str;
+    })
+
+    return header + '\n' + transactionsStr.join('\n')
   }
 }
 
--- statement.test.js
+++ statement.test.js
@@ -3,7 +3,7 @@
 const { Statement } = require('./statement');
 const { Transaction } = require('./transaction');
 
-describe('Transaction', () => {
+describe('Statement', () => {
   let statement;
 
   beforeEach(() => {
@@ -19,21 +19,21 @@ describe('Transaction', () => {
   });
 
   describe('makeTransaction', () => {
-    test('adds deposits to the balance', () => {
+    test('it adds deposits to the balance', () => {
       statement.makeTransaction(Transaction.deposit(Date.now(), 1000));
       statement.makeTransaction(Transaction.deposit(Date.now(), 2000));
 
       expect(statement.balance).toEqual(3000);
     });
 
-    test('subtracts withdrawals from the balance', () => {
+    test('it subtracts withdrawals from the balance', () => {
       statement.makeTransaction(Transaction.withdrawal(Date.now(), 455));
       statement.makeTransaction(Transaction.withdrawal(Date.now(), 289));
 
       expect(statement.balance).toEqual(-744);
     });
 
-    test('can modify balance with both deposits and withdrawals', () => {
+    test('it can modify balance with both deposits and withdrawals', () => {
       statement.makeTransaction(Transaction.deposit(Date.now(), 789));
       statement.makeTransaction(Transaction.withdrawal(Date.now(), 213));
 
@@ -42,8 +42,33 @@ describe('Transaction', () => {
   });
 
   describe('print', () => {
-    test('can display a blank statement', () => {
-      expect(statement.print()).toEqual('date || credit || debit || balanc');
+    test('it can display a blank statement', () => {
+      expect(statement.print()).toEqual('date || credit || debit || balance\n');
+    });
+
+    test('it can diplay all deposits and withdrawals in reverse chronological order', ()=> {
+      const firstTranDate = new Date(2019, 10, 22);
+      const secondTranDate = new Date(2019, 10, 24);
+
+      statement.makeTransaction(Transaction.deposit(firstTranDate, 1000));
+      statement.makeTransaction(Transaction.withdrawal(secondTranDate, 150));
+
+      const expectedStatment = 'date || credit || debit || balance\n24/10/2019 || 0 || 150 || 850\n22/10/2019 || 1000 || 0 || 1000';
+
+      expect(statement.print()).toEqual(expectedStatment);
+    });
+
+    test('it gives the same result if called multiple times', () => {
+      const firstTranDate = new Date(2019, 10, 22);
+      const secondTranDate = new Date(2019, 10, 24);
+
+      statement.makeTransaction(Transaction.deposit(firstTranDate, 1000));
+      statement.makeTransaction(Transaction.withdrawal(secondTranDate, 150));
+
+      const expectedStatment = 'date || credit || debit || balance\n24/10/2019 || 0 || 150 || 850\n22/10/2019 || 1000 || 0 || 1000';
+
+      expect(statement.print()).toEqual(expectedStatment);
+      expect(statement.print()).toEqual(expectedStatment);
     });
   });
 });
